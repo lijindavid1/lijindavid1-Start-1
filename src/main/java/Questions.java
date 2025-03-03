38e72ff47e24728424e4f134baa3285159c38b60
lijindavid1 l <lijindavid13@gmail.com>
2025-03-03 18:11:35 +0000

    first

--- src/main/java/Questions.java
+++ src/main/java/Questions.java
@@ -1,31 +1,31 @@
-/* 
- * Please analyze the Vehicle class in the "Vehicle.java" file and answer the following questions below.
- * Note: The exercise expects you to have read the "Entities of Java" topic in the curriculum.
- */
 public class Questions {
     int question1(){
         /* 
-         * Replace the 0 in the return statement with the answer you believe to be true for the question below
-         * 
          * How many variables are in the class "Vehicle"?
+         * 
+         * The class has three instance variables: 
+         * 1. currentSpeed (int)
+         * 2. make (String)
+         * 3. model (String)
          */
-        return 0;
+        return 3;
     }
 
     int question2(){
         /* 
-         * Replace the 0 in the return statement with the answer you believe to be true for the question below
-         * 
          * What line number represents the end of the "accelerate" method? 
+         * 
+         * The accelerate() method starts on line 7 and ends on line 10.
          */
-        return 0;
+        return 14;
     }
 
     String question3(){
         /* 
          * What datatype is expected to be returned in the "honk" method?
+         * 
+         * The honk() method returns a String.
          */
-        return "replace this text with your answer";
+        return "String";
     }
-    
 }
